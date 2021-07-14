30ad25e1ea8393ebd4c13927db0d60152cf97c7a
7Dany6 <dany.sulimov@yandex.ru>
2021-07-14 12:25:18 +0300

    Second commit

---
+++ Class_instances.py
@@ -0,0 +1,21 @@
+"""
+Here are the main points of class instances
+"""
+
+"""
+Write the class Movie and define the class constructor with such parameters as the title, director, and the year of release.
+
+In the corresponding variables defined below, create such objects as "Titanic" (James Cameron, 1997), "Star Wars" (George Lucas, 1977), and "Fight Club" (David Fincher, 1999).
+"""
+#SOLUTION
+class Movie:
+    def __init__(self, title, director, year):
+        self.title = title
+        self.director = director
+        self.year = year
+
+
+
+titanic = Movie("Titanic", "James Cameron", 1997)
+star_wars = Movie("Star Wars", "George Lucas", 1977)
+fight_club = Movie("Fight Club", "David Fincher", 1999)
