a1426c6a636b78a49a0280aacd01c95ae6ffe576
7Dany6 <dany.sulimov@yandex.ru>
2021-07-14 16:41:02 +0300

    Third commit

--- Magic methods.py
+++ Magic methods.py
@@ -1,5 +1,6 @@
 """
 There are different ways to enrich the functionality of your classes in Python. One of them is creating custom methods which you've already learned about.
 Another way, the one that we'll cover in this topic, is using "magic" methods.
+What is more, believe it or not but I'm sure you've encountered them previously. Don't believe?
 Dive into magic methods of object-oriented programming in Python right now!
 """
