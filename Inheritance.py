daf97e36ca2c7b3f073d22813475599069fdb4df
7Dany6 <dany.sulimov@yandex.ru>
2021-07-15 19:26:15 +0300

    Fourth commit

--- Inheritance.py
+++ Inheritance.py
@@ -1,3 +1,7 @@
 """
 This topic relates to advanced OOP. So, feel confident to explore and refresh your knowledge!
 """
+"""
+Inheritance is a mechanism that allows classes to inherit methods or properties from other classes.
+Or, in other words, inheritance is a mechanism of deriving new classes from existing ones.
+"""
